605a5074b2e45303f790abff204b790027b46c75
MrTango <md@derico.de>
2023-06-04 10:35:40 +0200

    working draft frontend4classicui

---
+++ resources/frontend4classicui/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Contextnav from '$lib/contextnav.svelte';
+	// import { PUBLIC_BASE_URL } from '$env/static/public';
+	// import Renderblock from '$lib/renderblock.svelte';
+
+	export let data = {};
+</script>
+
+<!-- {@debug data} -->
+<nav>
+	<ul>
+		{#each data.navigation as navItem}
+			<li><a href={navItem.href}>{navItem.title}</a></li>
+		{/each}
+	</ul>
+</nav>
+
+<Contextnav contextNav={data.contextnavigation} />
+<h1>root</h1>
+<h1>{data.title}</h1>
+<p>{data.description}</p>
+<!-- {#each data.blocks_layout.items as blockLayout}
+  <RenderBlock id={blockLayout} data={data} />
+{/each} -->
